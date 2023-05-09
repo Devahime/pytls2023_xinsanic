6a0c65d521b4a1167a5c3485cfea02a84a5216d1
xinsanic <xinsanic@mendelu.cz>
2023-05-09 19:43:54 +0200

    added #main and  #result in style.css

--- app.py
+++ app.py
@@ -13,12 +13,15 @@ if __name__ == '__main__':
 
 @app.route('/home' ,methods=['GET', 'POST'])
 def home():
+    bmi=None
     if request.method == 'POST':
         gender = (request.form['gender'])
         age = float(request.form['age'])
         height = float(request.form['height'])
         weight = float(request.form['weight'])
         bmi = weight / ((height / 100) ** 2)
+        if bmi <= 18,59:
+            print(Underweight)
     return render_template('index.html', bmi=bmi)
 
 
