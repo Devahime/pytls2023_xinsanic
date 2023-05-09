96bbbc5567361f1b1272c03f0568195a9d6f2544
xinsanic <xinsanic@mendelu.cz>
2023-05-09 19:58:58 +0200

    added if and elif statements (results), used jinja for printing results

--- app.py
+++ app.py
@@ -14,15 +14,24 @@ if __name__ == '__main__':
 @app.route('/home' ,methods=['GET', 'POST'])
 def home():
     bmi=None
+    bmitype=None
     if request.method == 'POST':
         gender = (request.form['gender'])
         age = float(request.form['age'])
         height = float(request.form['height'])
         weight = float(request.form['weight'])
         bmi = weight / ((height / 100) ** 2)
-        if bmi <= 18,59:
-            print(Underweight)
-    return render_template('index.html', bmi=bmi)
+        if bmi <= 18.59:
+            bmitype="Underweight"
+        elif bmi > 18.60 and bmi < 24.99:
+            bmitype="Normal"
+        elif bmi > 25 and bmi < 29.99:
+            bmitype="Overweight"
+        elif bmi > 30:
+            bmitype="Obese"
+
+
+    return render_template('index.html', bmi=bmi, bmitype=bmitype)
 
 
 
