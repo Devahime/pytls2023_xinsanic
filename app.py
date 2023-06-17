dd6eda90c93350e6fa77169970c96c7ae13d7151
xinsanic <xinsanic@mendelu.cz>
2023-06-17 19:08:24 +0200

    added link so its faster

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "BMI calculator"
+    return '<a href="http://127.0.0.1:5000/home"> BMI calculator</a>'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
